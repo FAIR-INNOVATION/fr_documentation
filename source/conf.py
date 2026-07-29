b506a75c44ecf399e7f28484134efebbec16b576
zhangJun2023 <zhangjun@frtech.fr>
2026-07-29 09:20:00 +0800

    feat:更改lfs使用方法

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '3.9.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark']
+extensions = ['recommonmark','sphinx_lfs_content']
 
 templates_path = ['_templates']
 exclude_patterns = []
