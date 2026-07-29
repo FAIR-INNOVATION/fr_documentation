b34a12e667ccebb6a2ebfa6e038ddf6edf6ca590
zhangJun2023 <zhangjun@frtech.fr>
2026-07-29 10:01:58 +0800

    feat:更改lfs使用方法

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '3.9.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = ['myst-parser']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
